686d1e6c46e7c5a033ef820fbe895c24374647f6
Stephen <stephen@whitebolt.net>
2016-04-06 10:26:50 +0100

    Start using ES5/ES6 shims instead

--- src/index.js
+++ src/index.js
@@ -3,30 +3,6 @@
 
 	angular.module("bolt", []);
 
-	if (typeof Object.assign != 'function') {
-		(function () {
-			Object.assign = function (target) {
-				'use strict';
-				if (target === undefined || target === null) {
-					throw new TypeError("Cannot convert undefined or null to object");
-				}
-
-				var output = Object(target);
-				for (var index = 1; index < arguments.length; index++) {
-					var source = arguments[index];
-					if (source !== undefined && source !== null) {
-						for (var nextKey in source) {
-							if (source.hasOwnProperty(nextKey)) {
-								output[nextKey] = source[nextKey];
-							}
-						}
-					}
-				}
-				return output;
-			};
-		})();
-	}
-
 	angular.element(document).ready(function() {
 		angular.forEach(angular.element.find("[wb-app]"), function(node) {
 			angular.bootstrap(node, [angular.element(node).attr("wb-app")]);
