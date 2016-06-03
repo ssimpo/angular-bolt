671b6fb520fab79d759812fb489a49fe5a58fa99
Stephen <stephen@whitebolt.net>
2016-06-03 14:09:43 +0100

    Add promise return to apply and refactor report
    Report now logs what has changed.  Apply returns a promise fulfilled on done.

--- src/bolt.js
+++ src/bolt.js
@@ -1,104 +1,110 @@
 angular.module("bolt").factory("$bolt", [
 	"$timeout",
-	function($timeout) {
-		"use strict";
+	"$q",
 
-		function shallowCopy(...args) {
-			args.unshift({});
-			const copy = Object.assign.apply(Object, args);
+function($timeout, $q) {
+	"use strict";
 
-			angular.forEach(copy, (value, key) => {
-				if (value === undefined) {
-					delete copy[key];
-				}
+	function shallowCopy(...args) {
+		args.unshift({});
+		const copy = Object.assign.apply(Object, args);
+
+		angular.forEach(copy, (value, key) => {
+			if (value === undefined) {
+				delete copy[key];
+			}
+		});
+
+		return copy;
+	}
+
+	function apply(options){
+		if (options.value.then) {
+			options.value.then(function(value) {
+				options.value = value;
+				apply(options);
 			});
+		} else {
+			const controller = options.controller || options.scope[options.scopeName];
+			const scope = options.scope || controller.parent;
 
-			return copy;
-		}
-
-		function apply(options){
-			if (options.value.then) {
-				options.value.then(function(value) {
-					options.value = value;
-					apply(options);
-				});
-			} else {
-				const controller = options.controller || options.scope[options.scopeName];
-
+			return $q(resolve => {
 				$timeout(function(){
-					options.scope.$apply(function(){
+					scope.$apply(function(){
 						controller[options.attributeName] = options.value;
+						resolve(options.value);
 						if (options.callback) {
 							options.callback(options.value);
 						}
 					});
 				});
-			}
-		}
-
-		/**
-		 * @todo Does this work? .fill() being used!
-		 */
-		function fill(start, end) {
-			return Array(end - start + 1).fill().map((i, n) => start + n);
-		}
-
-		function chunk(ary, chunkSize) {
-			return ary.map((item, index) =>
-				index % chunkSize === 0 ? ary.slice(index, index + chunkSize) : null
-			).filter(item => item);
-		}
-
-		function shuffle(ary) {
-			let currentIndex = ary.length, temporaryValue, randomIndex;
-
-			while (0 !== currentIndex) {
-				randomIndex = Math.floor(Math.random() * currentIndex);
-				currentIndex -= 1;
-				temporaryValue = ary[currentIndex];
-				ary[currentIndex] = ary[randomIndex];
-				ary[randomIndex] = temporaryValue;
-			}
-
-			return ary;
-		}
-
-		function forN(N) {
-			return Array.apply(null, {length: N}).map(Number.call, Number);
-		}
-
-		function forOwn(obj, iterator) {
-			Object.keys(obj).forEach(key => iterator(obj[key], key));
-		}
-
-		function forOwnMap(obj, iterator) {
-			let mapped = {};
-			Object.keys(obj).forEach(key => {
-				Object.assign(mapped, iterator(obj[key], key));
 			});
-			return mapped;
+		}
+	}
+
+	/**
+	 * @todo Does this work? .fill() being used!
+	 */
+	function fill(start, end) {
+		return Array(end - start + 1).fill().map((i, n) => start + n);
+	}
+
+	function chunk(ary, chunkSize) {
+		return ary.map((item, index) =>
+			index % chunkSize === 0 ? ary.slice(index, index + chunkSize) : null
+		).filter(item => item);
+	}
+
+	function shuffle(ary) {
+		let currentIndex = ary.length, temporaryValue, randomIndex;
+
+		while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			temporaryValue = ary[currentIndex];
+			ary[currentIndex] = ary[randomIndex];
+			ary[randomIndex] = temporaryValue;
 		}
 
-		function forOwnFilter(obj, iterator) {
-			let filtered = Object.assign({}, obj);
-			Object.keys(obj).forEach(key => {
-				if (!iterator(obj[key], key)) {
-					delete  filtered[key];
-				}
-			});
-			return filtered;
-		}
+		return ary;
+	}
 
-		function forOwnMapArray(obj, iterator) {
-			return Object.keys(obj).map(key => iterator(obj[key], key));
-		}
+	function forN(N) {
+		return Array.apply(null, {length: N}).map(Number.call, Number);
+	}
 
-		function forOwnFilterArray(obj, iterator) {
-			return Object.values(forOwnFilter(obj, iterator))
-		}
+	function forOwn(obj, iterator) {
+		Object.keys(obj).forEach(key => iterator(obj[key], key));
+	}
 
-		return {
-			apply, chunk, fill, forN, shallowCopy, shuffle, forOwn, forOwnMap,
-			forOwnMapArray, forOwnFilter, forOwnFilterArray
-		};
-	}]);
+	function forOwnMap(obj, iterator) {
+		let mapped = {};
+		Object.keys(obj).forEach(key => {
+			Object.assign(mapped, iterator(obj[key], key));
+		});
+		return mapped;
+	}
+
+	function forOwnFilter(obj, iterator) {
+		let filtered = Object.assign({}, obj);
+		Object.keys(obj).forEach(key => {
+			if (!iterator(obj[key], key)) {
+				delete  filtered[key];
+			}
+		});
+		return filtered;
+	}
+
+	function forOwnMapArray(obj, iterator) {
+		return Object.keys(obj).map(key => iterator(obj[key], key));
+	}
+
+	function forOwnFilterArray(obj, iterator) {
+		return Object.values(forOwnFilter(obj, iterator))
+	}
+
+	return {
+		apply, chunk, fill, forN, shallowCopy, shuffle, forOwn, forOwnMap,
+		forOwnMapArray, forOwnFilter, forOwnFilterArray
+	};
+}]);
--- src/watcher.js
+++ src/watcher.js
@@ -3,15 +3,40 @@ angular.module("bolt").factory("boltWatcher", [
 function() {
 	"use strict";
 
+	const controllers = new WeakMap();
+
+	function getPreviousWatchers(controller) {
+		if (!controllers.has(controller)) controllers.set(controller, new Map());
+		return controllers.get(controller);
+	}
+
+	function getWatcherChangeMap(watchers, previous) {
+		let changed = new Map();
+		angular.forEach(watchers, (value, key) => {
+			if (!previous.has(key)) {
+				changed.set(key, true);
+			} else {
+				if (previous[key] != value) changed.set(key, true);
+			}
+		});
+		return changed;
+	}
+
 	function report(options) {
 		const controller = options.controller || options.scope[options.scopeName];
 		const toWatch = options.toWatch || options.toObserve;
 
+		let previous = getPreviousWatchers(controller);
 		let currentWatcher = () => getWatcherObject(toWatch, controller);
 		let trigger = (watchers = currentWatcher()) => options.callback(watchers, options);
 
 		return Object.assign(
-			options.scope.$watch(currentWatcher, trigger, true),
+			options.scope.$watch(currentWatcher, watchers => {
+				let changed = getWatcherChangeMap(watchers, previous);
+				watchers.hasChanged = (key) => changed.has(key);
+				controllers.set(controller, watchers);
+				trigger(watchers);
+			}, true),
 			{trigger}
 		);
 	}
