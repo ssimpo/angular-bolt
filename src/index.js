8e61f727425994470120589097e05c207467b314
Stephen <stephen@whitebolt.net>
2016-06-10 07:48:07 +0100

    Remove bootstrapper
    This is not needed as using SystemJs.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-(function() {
-	"use strict";
-
-	angular.module("bolt", []);
-
-	angular.element(document).ready(() =>
-		angular.forEach(angular.element.find("[wb-app]"), node =>
-			angular.bootstrap(node, [angular.element(node).attr("wb-app")])
-		)
-	);
-})();
+angular.module("bolt", []);
